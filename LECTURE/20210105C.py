eac8fe7875470e114689431829e790ac736919be
Supreme-YS <dudtjr1225@gmail.com>
2021-01-05 09:28:44 +0900

    update

--- LECTURE/20210105C.py
+++ LECTURE/20210105C.py
@@ -1,6 +1,18 @@
 # Built-in-type
 # 변수 정의 및 생성
+# 블럭 단위 실행 = Shift + Alt + E
 intValue = 123
 floatValue = 3.14
 boolValue = True
 strValue = 'Supreme-YS'
+
+print( type(intValue), type(floatValue), type(boolValue), type(strValue))
+
+# type casting
+numStr = '720'
+numNum = 100
+print(int(numStr) + numNum)
+print(numStr + str(numNum))
+
+year = "2021"
+print(int(year) - 1)
