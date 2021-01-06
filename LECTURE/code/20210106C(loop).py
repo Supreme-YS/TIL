411d95a6ffff4b2981426b6b711acd5cf79107e1
Supreme-YS <dudtjr1225@gmail.com>
2021-01-06 16:06:53 +0900

    20210106 TIL

--- LECTURE/code/20210106C(loop).py
+++ LECTURE/code/20210106C(loop).py
@@ -28,7 +28,30 @@ userDict = {'name' : 'Supreme-YS', 'gender' : 'm'}
 for (key, value) in userDict.items() :
     print('{}, {}'.format(key, value))
 
+userSum = 0
 scores = [100, 50, 80, 90, 70, 60]
 print(len(scores))
-for score in range(len(scores)):
-    print(score, end = '\t')
+for idx in range(len(scores)):
+    userSum += scores[idx]
+
+# %d, %s, %f
+print('scores 합 %d' % userSum)
+
+# list comprehension
+'''
+[ for 구문을 통한 반복적인 표현식을 이용할 수 있다 if 조건도 사용 가능 ]
+'''
+
+userList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+userList02 = [ tmp ** 2 for tmp in userList]
+print('comprehension - ', userList02)
+
+# userList에서 짝수만 가져와서 실행식을 시
+userList03 = [tmp ** 2 for tmp in userList if tmp % 2 == 0]
+print('comprehension - ', userList03)
+
+# dict에서도 사용 가능
+userDict = {'Test' + str(tmp) : tmp ** 2 for tmp in range(1, 10)}
+print(userDict)
+userDict = {'Test' + str(tmp) : tmp ** 2 for tmp in range(1, 10) if tmp % 2 == 0}
+print(userDict)
