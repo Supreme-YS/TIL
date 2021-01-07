50c51ae845c6815f21ce9eea5965bcb91c6c8c94
Supreme-YS <dudtjr1225@gmail.com>
2021-01-07 14:07:07 +0900

    20210107 TIL

---
+++ LECTURE/20210107C(function).py
@@ -0,0 +1,17 @@
+# python function
+'''
+
+함수는 가독성을 높이기 위한 방법으로
+하나 이상의 본문을 가지는 코드는 함수로 정의하는 것이 좋다.
+내장 함수 | 사용자 정의 함수
+함수를 정의할 때는 def 키워드를 이용해서 함수를 정의
+'''
+
+# user define function
+'''
+def returnType functionName([arg01, arg02]) :
+    statement
+    return value(built-in Type)
+'''
+def userPrint() :
+    print('userPrint')
---
+++ LECTURE/python/packageFunction.py
@@ -0,0 +1,2 @@
+def printCoins():
+    print('bitcoin')
