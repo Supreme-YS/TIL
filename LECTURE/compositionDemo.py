a5afa62080f9723d97b64ada0d99ca75303c168f
Supreme-YS <dudtjr1225@gmail.com>
2021-01-13 10:21:37 +0900

    20210113 TIL

--- LECTURE/compositionDemo.py
+++ LECTURE/compositionDemo.py
@@ -47,3 +47,49 @@ class UserCalc : # Calc02의 multiply 기능만 쓰고 싶다. (상속받지 않
 
 calc = UserCalc(3, 4)
 print('calc multiply - ', calc.multiply())
+
+'''
+- 예외(Exception) - SyntaxError, TypeError, NameError, IndexError, KeyError ...
+- 처리할 수 있는 구문
+try    :
+    에러가 발생할 가능성이 있는 코드
+    정상코드
+    에러가 발생할 가능성이 있는 코드
+    정상코드
+except xxxxError :
+    1. 프로그램의 흐름을 정상으로 컨트롤
+    2. 예외발생의 디버킹
+    3. 로그파일 만들어서 예외정보를 저장
+
+    발생된 에러를 잡기위한 객체 정의
+except xxxxError :
+    발생된 에러를 잡기위한 객체 정의
+else   :
+    에러가 발생되지 않을 때 실행되는 블럭
+finally:
+    무조건 수행
+'''
+# SyntaxError
+## print('error)
+## print('end')
+
+# NameError
+## a = 10
+## b = 20
+## princ(c)
+
+# ZeroDivisionError
+## print(100/0)
+
+# IndexError
+## x = [1, 2, 3]
+## print(x[3])
+
+# KeyError
+## dict = {'name' : 'supreme', 'age' : 30, 'city' : 'gimpo'}
+## print(dict['hobby'])
+
+# 예외가 없는 것을 가정하고 프로그램 작성 -> 런타임 예외 발생시 예외 처리를 권장
+print(dict.get('name'))
+print('*' * 50)
+print('end')
