6f2172870194977439d87831aee9e8bd48d13c98
Supreme-YS <dudtjr1225@gmail.com>
2021-01-14 10:22:37 +0900

    20210114 TIL

--- LECTURE/streamExec.py
+++ LECTURE/streamExec.py
@@ -48,3 +48,33 @@ def pickleReader() :
     print('객체 직렬화된 파일을 로드하기')
     
 pickleReader()
+
+# 1
+# 단어가 줄 단위로 저장된 cnt_words.txt 파일에서 10자 이하인 단어의 갯수를 카운트하는
+# 함수를 구현하고 호출한다면?
+'''My answer'''
+def countWords() :
+    countUnder = []
+    with open(file = './word/cnt_words.txt', mode = 'r', encoding= 'utf-8') as file :
+        for line in file.readlines() :
+            line = line.strip('\n')
+            if len(line) <= 10 :
+                countUnder.append(line)
+    print('10자 이하의 단어의 갯수는 {}개 입니다.'.format(len(countUnder)))
+    print(countUnder)
+countWords()
+
+'''Teacher answer'''
+def wordCntFunc() :
+    wordList = []
+    with open(file = './word/cnt_words.txt', mode = 'r', encoding= 'utf-8') as file :
+        for word in file.readlines() :
+            word = word.strip('\n')
+            if len(word) <= 10 :
+                wordList.append(word)
+    print('10자 이하의 단어의 갯수는 {}개 입니다.'.format(len(wordList)))
+
+wordCntFunc()
+
+
+
