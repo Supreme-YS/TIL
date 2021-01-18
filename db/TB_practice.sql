3c4c14cf83ddaf17f78937230ae10f471da53d93
Supreme-YS <dudtjr1225@gmail.com>
2021-01-18 16:35:51 +0900

    20210118 TIL

--- db/TB_practice.sql
+++ db/TB_practice.sql
@@ -51,7 +51,32 @@ FROM TB_DEPARTMENT;
 --#10 02학번 전주 거주자들의 모임을 만들려고 한다. 휴학한 사람들은 제외한 재학중인 학생들의 학번, 이름, 주민번호를 출력하는 구문을 작성하시오.
 SELECT * --STUDENT_NO, STUDENT_NAME, STUDENT_SSN
 FROM TB_STUDENT
-WHERE ABSENCE_YN = 'N' AND STUDENT_ADDRESS LIKE '전주%' AND ENTRANCE_DATE LIKE '02%'
+WHERE ABSENCE_YN = 'N' AND STUDENT_ADDRESS LIKE '전주%' AND ENTRANCE_DATE LIKE '02%';
 
 --#ADDITIONAL SELECT 함수
+--#1 영어영문학과(학과코드 002) 학생들의 학번과 이름, 입학 년도를 입학 년도가 빠른 순으로 표시하는 SQL 문장을 작성하시오.( 단, 헤더는 "학번", "이름", "입학년도" 가 표시되도록 한다.)
+SELECT STUDENT_NO AS 학번,
+       STUDENT_NAME AS 이름,
+       ENTRANCE_DATE AS 입학년도
+FROM TB_STUDENT
+WHERE DEPARTMENT_NO = 002 ORDER BY ENTRANCE_DATE ASC;
+
+--#2 춘 기술대학교의 교수 중 이름이 세 글자가 아닌 교수가 한 명 있다고 한다. 그 교수의 이름과 주민번호를 화면에 출력하는 SQL 문장을 작성해 보자.
+--(* 이때 올바르게 작성한 SQL 문장의 결과 값이 예상과 다르게 나올 수 있다. 원인이 무엇일지 생각해볼 것)
+SELECT PROFESSOR_NAME, PROFESSOR_SSN
+FROM TB_PROFESSOR
+WHERE PROFESSOR_NAME NOT LIKE '___';
+
+--#3춘 기술대학교의 남자 교수들의 이름과 나이를 출력하는 SQL 문장을 작성하시오.
+--단 이때 나이가 적은 사람에서 많은 사람 순서로 화면에 출력되도록 만드시오.
+--(단, 교수 중 2000년 이후 출생자는 없으며 출력 헤더는 "교수이름", "나이"로 한다. 나이는 ‘만’으로 계산한다.)
+SELECT *
+FROM TB_PROFESSOR;
+
+SELECT PROFESSOR_NAME AS 교수이름,
+       PROFESSOR_SSN AS 나이
+FROM TB_PROFESSOR;
+WHERE 
+
+
 
