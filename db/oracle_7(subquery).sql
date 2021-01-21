b0d04323a6ec2c3a9f48e141af5039ab452b7b71
Supreme-YS <dudtjr1225@gmail.com>
2021-01-21 11:41:34 +0900

    20210121 TIL

--- db/oracle_7(subquery).sql
+++ db/oracle_7(subquery).sql
@@ -151,9 +151,125 @@ GROUP BY DEPT_NAME
 HAVING SUM(SALARY) = (SELECT MAX(SUM(SALARY))
                         FROM EMPLOYEE
                         GROUP BY DEPT_ID)
-
 --서브쿼리 만들기
 SELECT MAX(SUM(SALARY))
 FROM EMPLOYEE
 GROUP BY DEPT_ID;
+
+
+--IN, NOT IN, ANY, ALL 연산자를 다중행 서브쿼리에서 사용할 수 있다.
+
+SELECT EMP_NAME, JOB_ID, SALARY
+FROM EMPLOYEE
+WHERE SALARY IN ( SELECT MIN(SALARY)
+                    FROM EMPLOYEE
+                    GROUP BY DEPT_ID) ;
+
+SELECT EMP_ID,
+       EMP_NAME,
+       '관리자' AS 구분
+FROM EMPLOYEE
+WHERE EMP_ID IN (SELECT MGR_ID FROM EMPLOYEE )
+UNION
+SELECT EMP_ID,
+       EMP_NAME,
+       '직원' AS 구분
+FROM EMPLOYEE
+WHERE EMP_ID NOT IN ( SELECT MGR_ID FROM EMPLOYEE
+                      WHERE MGR_ID IS NOT NULL) --다중행 서브쿼리에서 NOT IN을 사용할 땐 NULL 값을 제거해줘야한다. 제거하지 않으면 전체를 NULL 값으로 보내기 때문에..
+
+--위 구현 코드를 다른 방식(UNION을 사용하지 않고) 으로 구현한다면?
+--MGR_ID 있으면 직원, 없으면 사원(DECODE, CASE~END)
+--#0 DECODE 방식
+SELECT EMP_ID,
+       EMP_NAME,
+       DECODE(MGR_ID, NULL , '관리자', '사원') AS 구분
+FROM EMPLOYEE
+       
+--#1 CASE ~ WHEN 방식
+SELECT EMP_ID,
+       EMP_NAME,
+       CASE WHEN EMP_ID IN (SELECT MGR_ID FROM EMPLOYEE) THEN '관리자' ELSE '직원' END
+FROM EMPLOYEE
+--#2 CASE
+SELECT EMP_ID,
+       EMP_NAME,
+       CASE
+           WHEN MGR_ID IS NOT NULL THEN '직원'
+       ELSE
+           '관리자'
+       END AS 구분
+FROM EMPLOYEE
+
+
+
+-- ANY와 ALL비교
+-- * > ANY 최소값보다 큰
+-- 2100000 보다 큰
+SELECT EMP_NAME,
+       SALARY
+FROM EMPLOYEE
+JOIN JOB USING (JOB_ID)
+WHERE JOB_TITLE = '대리'
+AND SALARY > ANY 
+                (SELECT SALARY
+                FROM EMPLOYEE
+                JOIN JOB USING (JOB_ID)
+                WHERE JOB_TITLE = '과장');
+
+-- * < ANY 최대값보다 작은
+-- 2600000 보다 작은
+SELECT EMP_NAME,
+       SALARY
+FROM EMPLOYEE
+JOIN JOB USING (JOB_ID)
+WHERE JOB_TITLE = '대리'
+AND SALARY < ANY 
+                (SELECT SALARY
+                FROM EMPLOYEE
+                JOIN JOB USING (JOB_ID)
+                WHERE JOB_TITLE = '과장');
+
+-- * > ALL 비교대상 중 최대값보다 큰
+-- 2600000 보다 큰
+SELECT EMP_NAME,
+       SALARY
+FROM EMPLOYEE
+JOIN JOB USING (JOB_ID)
+WHERE JOB_TITLE = '대리'
+AND SALARY > ALL 
+                (SELECT SALARY
+                FROM EMPLOYEE
+                JOIN JOB USING (JOB_ID)
+                WHERE JOB_TITLE = '과장');
+
+-- * < ALL 비교대상 중 최소값보다 작은
+-- 2100000 보다 작은
+SELECT EMP_NAME,
+       SALARY
+FROM EMPLOYEE
+JOIN JOB USING (JOB_ID)
+WHERE JOB_TITLE = '대리'
+AND SALARY < ALL
+                (SELECT SALARY
+                FROM EMPLOYEE
+                JOIN JOB USING (JOB_ID)
+                WHERE JOB_TITLE = '과장');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
