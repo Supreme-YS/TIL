fdd1581ab13e57f30b9a1df638e8e019b869d3b7
Supreme-YS <dudtjr1225@gmail.com>
2021-01-22 10:16:08 +0900

    20210122 TIL

--- db/oracle_8(table).sql
+++ db/oracle_8(table).sql
@@ -21,7 +21,9 @@ CREATE TABLE TEST_MEMBER(--테이블 생성
     PWD  VARCHAR2(50) NOT NULL, -- 널 값을 허용하지 않는 조건
     ADDR VARCHAR2(50) DEFAULT 'SEOUL', --DEFAULT 즉, 기본값으로 삽입되는 열
     PRIMARY KEY(ID) --TABLE 제약을 줄 수 있는 자리이며, NOT NULL(칼럼명)은 불가능 PRIMARY KEY(칼럼명)은 가능
-)
+) --#1 컬럼생성과 동시에 제약을 주는 방법
+  --#2 컬럼생성 후 테이블 레벨 제약 자리에서 칼럼에 제약을 줄 수 있다.
+
 
 INSERT INTO TEST_MEMBER(ID, PWD, ADDR) VALUES('YSSIM', 'YSSIM', 'SEOUL');
 INSERT INTO TEST_MEMBER VALUES('YSSIM', 'YSSIM', 'SEOUL'); --PRIMARY KEY로 설정해놨기 때문에 동일한 값 입력 불가능!
@@ -34,3 +36,25 @@ FROM TEST_MEMBER;
 -- 2. 제약조건 (NOT NULL - 컬럼 레벨 제약만 가능, UNIQUE, PRIMARY KEY, FOREIGEN KEY, CHECK)
 -- 2-1. 제약조건을 설정하는 방법 : 테이블 레벨, 컬럼 레벨
 
+CREATE TABLE TEST_MEMBER(
+    ID   VARCHAR2(50) PRIMARY KEY,
+    PWD  VARCHAR2(50) PRIMARY KEY,
+    ADDR VARCHAR2(50) DEFAULT 'SEOUL'
+) --기본키는 하나여야 한다. 두 개의 기본키는 허용하지 않는다.
+  --하지만 두 개를 잡아야 한다면?
+
+CREATE TABLE TEST_MEMBER(
+    ID   VARCHAR2(50),
+    PWD  VARCHAR2(50),
+    ADDR VARCHAR2(50) DEFAULT 'SEOUL',
+    PRIMARY KEY(ID, PWD) --이런 식으로 COMPOSITE 하여 만들 순 있다.
+)
+  
+--다중 컬럼 자체가 PRIMARY KEY 이기 때문에 아래처럼 입력이 가능하다.
+--(YSSIM , YSSIM) 조합1
+--(YSSIM , ADMIN) 조합2
+INSERT INTO TEST_MEMBER(ID, PWD, ADDR) VALUES('YSSIM', 'YSSIM', 'SEOUL');
+INSERT INTO TEST_MEMBER(ID, PWD, ADDR) VALUES('YSSIM', 'ADMIN', 'SEOUL'); 
+
+SELECT *
+FROM TEST_MEMBER;
