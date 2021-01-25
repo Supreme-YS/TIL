369bbacc67b26ad80e002f2074882a974d553939
Supreme-YS <dudtjr1225@gmail.com>
2021-01-25 13:32:35 +0900

    20210125 TIL

--- AIWEB/djangoWEB/HelloApp/views.py
+++ AIWEB/djangoWEB/HelloApp/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render, HttpResponse
 
 # urls에 정의한 함수 이름을 사용하여 함수를 views에서 만든다.
 def index(request):
-    return HttpResponse('*** 여기는 시작페이지 입니다. Hello World Guys. ***')
+    # return HttpResponse('*** 여기는 시작페이지 입니다. Hello World Guys. ***') # 이게 비효율적이라서, html template와 연결시킨다.
+    # template와 연동시킬 수 있게 하는 함수 render
+    return render(request, 'hello/index.html') # hello 라는 directory안에 있는 index html파일을 연동
