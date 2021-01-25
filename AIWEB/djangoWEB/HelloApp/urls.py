6decde0d3678b1a78670a83d725ed22bfed687d2
Supreme-YS <dudtjr1225@gmail.com>
2021-01-25 15:17:25 +0900

    20210125 TIL

--- AIWEB/djangoWEB/HelloApp/urls.py
+++ AIWEB/djangoWEB/HelloApp/urls.py
@@ -6,7 +6,7 @@ from HelloApp    import views
 urlpatterns = [
     # localhost:8000/hello/index
     path('index/', views.index),
-    path('baseball/', views.baseball),
+    path('baseball/', views.baseball, name = 'base'),
     path('football/', views.football),
     path('basketball/', views.basketball),
     path('login/', views.login, name = 'msg'),
