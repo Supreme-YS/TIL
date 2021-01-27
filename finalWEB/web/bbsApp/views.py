aab4fd1c7718b3538a527b443cfb35d4d24adbf3
Supreme-YS <dudtjr1225@gmail.com>
2021-01-27 11:42:08 +0900

    20210127 TIL

--- finalWEB/web/bbsApp/views.py
+++ finalWEB/web/bbsApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,4 +6,13 @@ def index(request) :
     return render(request, 'login.html')
 
 def loginProc(request) :
-    return render(request, 'home.html')
+    print('request - loginProc')
+    if request.method == 'GET' :
+        return redirect('index')
+    elif request.method == 'POST' :
+        id  = request.POST['id']
+        pwd = request.POST['pwd']
+        if id == 'yssim' and pwd == 'yssim' :
+            return render(request, 'home.html')
+        else :
+            return redirect('index')
