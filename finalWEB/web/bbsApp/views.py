03cb7e051130fcc0da972f95b8f0c1ecea10c490
Supreme-YS <dudtjr1225@gmail.com>
2021-01-27 14:23:57 +0900

    20210127 TIL

--- finalWEB/web/bbsApp/views.py
+++ finalWEB/web/bbsApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
 
@@ -12,7 +13,11 @@ def loginProc(request) :
     elif request.method == 'POST' :
         id  = request.POST['id']
         pwd = request.POST['pwd']
-        if id == 'yssim' and pwd == 'yssim' :
+        # select * from bbsuserregister where user_id = id and user_pwd = pwd
+        # orm class - table
+        user = BbsUserRegister.objects.get(user_id=id, user_pwd=pwd)
+        print('************ user result -', user)
+        if user is not None :
             return render(request, 'home.html')
         else :
             return redirect('index')
