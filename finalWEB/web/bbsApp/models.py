c7dfd567ca093fdd2879e9628af5357de07a96be
Supreme-YS <dudtjr1225@gmail.com>
2021-01-27 13:40:20 +0900

    20210127 TIL

--- finalWEB/web/bbsApp/models.py
+++ finalWEB/web/bbsApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class BbsUserRegister(models.Model) :
+    user_id = models.CharField(max_length=50)
+    user_pwd = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user_id+" , "+self.user_pwd+" , "+self.user_name
+
