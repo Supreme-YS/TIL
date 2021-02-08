6644e19c34fc63a8d212323364f2e0af7554b438
Supreme-YS <dudtjr1225@gmail.com>
2021-02-08 13:26:45 +0900

    20210208 TIL

--- codeup/codeup.py
+++ codeup/codeup.py
@@ -5,3 +5,34 @@ print('Hello')
 print('Hello World')
 
 #1003
+print('Hello')
+print('World')
+
+print('Hello\nWorld')
+
+#1004
+print("'Hello'")
+
+#1005
+print('"Hello World"')
+
+#1006
+print('"!@#$%^&*()"')
+
+#1007
+print('"C:\Download\hello.cpp"')
+
+#1008
+print("\u250C\u252C\u2510")
+print("\u251C\u253C\u2524")
+print("\u2514\u2534\u2518")
+
+#1010
+a = int(input())
+print(a)
+
+#1011
+a = str(input())
+print(a)
+
+#1012
