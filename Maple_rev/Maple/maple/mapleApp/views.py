9c4227476436a3adfdc9a8f2578275946416abe7
Supreme-YS <dudtjr1225@gmail.com>
2021-02-09 21:42:43 +0900

    20210209 TIL

--- Maple_rev/Maple/maple/mapleApp/views.py
+++ Maple_rev/Maple/maple/mapleApp/views.py
@@ -50,58 +50,72 @@ def order(request):
 
 
 def saveOrder(request) :
-    mID = request.POST.get('menuId','0')
-    mOrderno = request.POST.get('orderNo', '0')
-    mPrice = request.POST.get('mPrice', 0)
-    mQty = request.POST.get('mValue', 0)
-    print('request  saveOrder- ', mID ,mOrderno, mPrice, mQty)
+    # ---------------------------
+    # 1. orderno 채번
+    # ---------------------------
+    today = datetime.today()
+    todate = datetime.strftime(today, '%Y-%m-%d')
 
+    # 주문번호 : RYYYYMMDD + '00001'
+    new_orderno = 'R' + datetime.strftime(today, '%Y%m%d') + '00001'
+
+    # 주문번호 : 당일 주문  조회
+    od = Order.objects.values('orderdate').filter(orderdate=todate).annotate(max_orderno=Max('orderno'))
+
+    # 주문번호 : 당일 주문 있으면  다음번호 채번
+    if od:
+        a = od[0]['max_orderno']
+        new_orderno = 'R' + datetime.strftime(today, '%Y%m%d') + str(int(a[9:]) + 1).zfill(5)
+    print('new_orderno - ', new_orderno)
+
+    # -------------------------------
+    # 2. 날짜분할(orderdate, ordertime)
+    # -------------------------------
+
+    # 주문날짜(orderdate)
+    mDate = datetime.strftime(today, '%Y-%m-%d')
+
+    # 주문시간(ordertime)
+    mTime = datetime.strftime(today, '%H:%M:%S')
+
+    # -------------------------------
+    # 3. Order 테이블 저장
+    # -------------------------------
+    mID = request.POST.getlist('hmid[]')
+    mPrice = request.POST.getlist('hprice[]')
+    mQty = request.POST.getlist('hqty[]')
+    print('mID ', mID)
+    print('mPrice ', mPrice)
+    print('mQty ', mQty)
     print('-------------------------')
-    menu = Menu.objects.get(menuid=mID)
-    print(menu)
-    # order = Order.objects.get(orderno=mOrderno)
-    # savodd = OrderDetail(
-    #     menuid = menu.menuid,
-    #     orderno = order.orderno,
-    #     price = mPrice,
-    #     qty = mQty
-    # )
-    # print(savodd)
-    # savodd.save()
-    #
-    # print('------------------------')
 
-    # mOrderno = request.POST.get('mOrderNo', '0')
-    # mOrderDate = request.POST.get('now', '2021-02-06 10:01:00')
-    # mPayment = request.POST.get('', 'card')
-    # mStatus = request.POST.get('', 'order')
-    # savod = Order(
-    #     orderno = mOrderno,
-    #     orderdate = mOrderDate,
-    #     payment = mPayment,
-    #     status = mStatus,
-    # )
-    # savod.save()
 
+    # mDate = request.POST.get('hdate')
+    # mTime = request.POST.get('htime')
+    mPay = request.POST.get('hpay')
+    mStat = request.POST.get('hstat')
+    ord = Order(orderno=new_orderno, orderdate=mDate, ordertime=mTime, payment=mPay,
+                               status = mStat)
+    ord.save()
+    print(ord)
+
+    # -------------------------------
+    # 4. OrderDetail 테이블 저장
+    # -------------------------------
+    for i in range(len(mID)) :
+        menu = Menu.objects.get(menuid=mID[i])
+        # print('menu',menu)
+
+    for i in range(len(mID)) :
+        ordd = OrderDetail(
+            orderno = ord,
+            menuid = menu,
+            price = mPrice[i],
+            qty = mQty[i],
+        )
+        ordd.save()
+    # ---------------------------------
     return redirect('order')
-
-def menu(request):
-    return redirect('serchmenu')
-
-
-def serchmenu(request):
-    print('*> serchmenu :')
-    menus = Menu.objects.all()
-    print('menus', menus)
-    # title  writer  content  regdata  viewcnt
-    # print('*>producs -', type(producs), producs)
-    context = {'menus': menus}
-
-    menuName = request.POST.get('menuName', '0')
-
-
-
-    return render(request, 'menu.html', context)
 
 
 # 샘플CRUD - 입력
