00ea44f607ec3b87defe62166f548c6f25920def
Supreme-YS <dudtjr1225@gmail.com>
2022-04-07 12:34:47 +0900

    add Servlet dependency

---
+++ java-servlet-demo/src/main/java/me/supreme/HelloServlet.java
@@ -0,0 +1,31 @@
+package me.supreme;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        System.out.println("Servlet Init");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet");
+        resp.getWriter().println("<html>");
+        resp.getWriter().println("<head>");
+        resp.getWriter().println("<body>");
+        resp.getWriter().println("<h1>Hello Servlet</h1>");
+        resp.getWriter().println("</body>");
+        resp.getWriter().println("</head>");
+        resp.getWriter().println("</html>");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Servlet Destroyed");
+    }
+}
